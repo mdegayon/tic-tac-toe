af224bbeafc27794dd0d6ce25f5339b34e5f2225
M Degayon <mdegayon@gmail.com>
2025-11-06 21:51:01 +0100

    feat: Apply NES-style container to Player component

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Player({player}) {
   return (
-    <div className="Player">
-      <i className={player.icon}></i>
-      <ul>
-        <li>Name: {player.name}</li>
+    <div className="Player nes-container is-rounded">
+        <i className={player.icon}></i>
+        <ul>
+            <li>Name: {player.name}</li>
         <li>Victories: {player.victories}</li>
       </ul>
     </div>
