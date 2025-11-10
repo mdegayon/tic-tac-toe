104c2e04c4a5bd951ee0f0249e094e93b376eb5f
M Degayon <mdegayon@gmail.com>
2025-11-10 22:26:21 +0100

    feat: Add core Storybook configuration

---
+++ .storybook/main.js
@@ -0,0 +1,33 @@
+module.exports = {
+  stories: [
+    "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  },
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', { targets: "defaults" }],
+            ['@babel/preset-react', { runtime: 'automatic' }]
+          ]
+        }
+      }
+    });
+    return config;
+  }
+}
